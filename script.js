10c738a7056ce085fd8d5990b4f1b58ae4482d88
Leonardo Quadros Fragozo <leo.fragozo.dev@gmail.com>
2023-02-27 15:32:43 -0300

    Add play game function

--- script.js
+++ script.js
@@ -15,7 +15,38 @@ function getPcOption(arr) {
 }
 
 let options = ['Paper', 'Rock', 'Scissors'];
-let computer_option = getPcOption(options);
-console.log("The computer choose was:" + " " + computer_option);
+let computerOption = getPcOption(options);
+console.log("The computer choose was:" + " " + computerOption);
 
+getPcOption();
+
+function playGame(userOption, computerOption) {
+  let log = '';
+
+  if (userOption === 'Rock') {
+    if (computerOption === 'Paper') {
+      log = 'You Lose! Paper beats Rock';
+    } else if (computerOption === 'Scissors') {
+      log = 'You Win! Rock beats Scissors';
+    } else {
+      log = "It's a tie";
+    }
+  } else if (userOption === 'Paper') {
+    if (computerOption === 'Scissors') {
+      log = 'You Lose! Scissors beats Paper';
+    } else if (computerOption === 'Rock') {
+      log = 'You Win! Paper beats Rock';
+    } else {
+      log = "It's a tie";
+    }
+  } else if (userOption === 'Scissors') {
+    if (computerOption === 'Rock') {
+      log = 'You Lose! Rock beats Scissors';
+    } else if (computerOption === 'Paper') {
+      log = 'You Win! Scissors beats Paper';
+    } else {
+      log = "It's a tie";
+      }
+    }
+  }
 }
