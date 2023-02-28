f454f542bbf0c262b1f7684b2ebbacb570b4e013
Leonardo Quadros Fragozo <leo.fragozo.dev@gmail.com>
2023-02-28 13:36:49 -0300

    Calling the function

--- script.js
+++ script.js
@@ -18,35 +18,37 @@ let options = ['Paper', 'Rock', 'Scissors'];
 let computerOption = getPcOption(options);
 console.log("The computer choose was:" + " " + computerOption);
 
-getPcOption(op);
+getPcOption(options);
 
 function playGame(userOption, computerOption) {
   let log = '';
 
   if (userOption === 'Rock') {
     if (computerOption === 'Paper') {
-      log = 'You Lose! Paper beats Rock';
+      console.log ("You Lose! Paper beats Rock");
     } else if (computerOption === 'Scissors') {
-      log = 'You Win! Rock beats Scissors';
+      console.log ("You Win! Rock beats Scissors");
     } else {
-      log = "It's a tie";
+      console.log ("It's a tie");
     }
   } else if (userOption === 'Paper') {
     if (computerOption === 'Scissors') {
-      log = 'You Lose! Scissors beats Paper';
+      console.log ("You Lose! Scissors beats Paper");
     } else if (computerOption === 'Rock') {
-      log = 'You Win! Paper beats Rock';
+      console.log ("You Win! Paper beats Rock");
     } else {
-      log = "It's a tie";
+      console.log ("It's a tie");
     }
   } else if (userOption === 'Scissors') {
     if (computerOption === 'Rock') {
-      log = 'You Lose! Rock beats Scissors';
+      console.log ("You Lose! Rock beats Scissors");
     } else if (computerOption === 'Paper') {
-      log = 'You Win! Scissors beats Paper';
+      console.log ("You Win! Scissors beats Paper");
     } else {
-      log = "It's a tie";
+      console.log("It's a tie");
       }
     }
+    console.log("The result is ");
   }
+  playGame();
 }
